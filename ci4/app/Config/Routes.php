cf0493360f7f76ebdb10951340fbc2b49f366135
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-02-14 19:48:46 +0800

    Added headers routes etc

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -5,4 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+use App\Controllers\Pages;
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
 $routes->get('/', 'Home::index');
