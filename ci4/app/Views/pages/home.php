ace00476ee06ea1b4a4bcd127143e1691c58da82
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-02-17 22:49:22 +0800

    Re added

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -1,4 +1,17 @@
-<?php echo link_tag('css/website.css')?>
+<link rel="stylesheet" href="css/website.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!--open sans font-->
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&family=Roboto&display=swap" rel="stylesheet">
+<!--logo font-->
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+
+
+
+<head>
+	<title>shortscouture!</title>
+	<link rel="icon" type="image/xicon" href="images/chitoge.ico">
+</head>
 <body class = "bg">
  
   <!--navbar-->
