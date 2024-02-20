e09e7869d152ea8ec7389e659247f3cc7ab18a48
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-02-20 18:50:57 +0800

    More code so news displays

--- ci4/app/Config/Filters.php
+++ ci4/app/Config/Filters.php
@@ -56,7 +56,7 @@ class Filters extends BaseConfig
      * permits any HTTP method to access a controller. Accessing the controller
      * with a method you don't expect could bypass the filter.
      */
-    public array $methods = [];
+    public array $methods = ['csrf'];
 
     /**
      * List of filter aliases that should run on any
@@ -66,4 +66,5 @@ class Filters extends BaseConfig
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
     public array $filters = [];
+
 }
--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -11,7 +11,9 @@ use App\Controllers\News;
 use App\Controllers\Pages;
 
 
-$routes->get('news', [News::class, 'index']);           
+$routes->get('news', [News::class, 'index']);   
+$routes->get('news/new', [News::class, 'new']); // Add this line
+$routes->post('news', [News::class, 'create']);         
 $routes->get('news/(:segment)', [News::class, 'show']); 
 
 $routes->get('pages', [Pages::class, 'index']);
